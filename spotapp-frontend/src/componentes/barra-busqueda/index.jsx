07661d228cac72a1cfdffede4b28ac38b58445ec
Hazael Spinoza <120431664+hazaelbc@users.noreply.github.com>
2026-02-16 23:49:42 -0800

    correcciones barra de busqueda con respecto a continuidad

--- spotapp-frontend/src/componentes/barra-busqueda/index.jsx
+++ spotapp-frontend/src/componentes/barra-busqueda/index.jsx
@@ -16,19 +16,19 @@ export const BarraBusqueda = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full max-w-2xl mx-auto px-2 sm:px-4">
+    <form onSubmit={handleSubmit} className="w-full">
       <div className="relative flex items-center">
         <input
           type="text"
           value={searchTerm}
           onChange={handleChange}
           placeholder="Buscar en la Lobby..."
-          className="w-full px-3 py-2 pl-5 mt-4 sm:px-4 sm:py-3 sm:pl-7 sm:mt-6 text-sm sm:text-base text-gray-700 bg-white border border-gray-300 rounded-3xl focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent shadow-sm transition duration-200"
+          className="w-full px-3 py-2 pl-5 sm:px-4 sm:py-3 sm:pl-7 text-sm sm:text-base text-gray-700 bg-white border border-gray-300 rounded-3xl focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent shadow-sm transition duration-200"
         />
         
         <button
           type="submit"
-          className="absolute right-2 sm:right-4 top-1/2 -translate-y-1/2 flex items-center justify-center mt-2 sm:mt-3"
+          className="absolute right-2 sm:right-4 top-1/2 -translate-y-1/2 flex items-center justify-center"
         >
           <FiSearch className="text-gray-400 text-2xl sm:text-3xl" />
         </button>
